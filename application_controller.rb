c567379ddc185984b32029e1cf0152fdf908a25e
sgudur9 <18sgudur.stem@gmail.com>
2018-08-02 12:51:03 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,13 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
   get '/' do
-    erb :index
+    erb:exercise
   end
-
+  
+post '/order' do
+  "My name is #{params[:name]}. I would like a #{params[:espresso]} shot of espresso with #{params[:milk]} milk. Please ensure that my beverage is #{params[:pref]}. Don't forget to add #{params[:sugar]} packets of sugar! :)"
+end
 end
